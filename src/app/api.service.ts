0f11bfd5564c506247381b6c2f319dd4011e1348
Daryna Kondor <condordaryna@gmail.com>
2023-04-30 20:07:54 +0300

    add users data to the page

---
+++ src/app/api.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from '@angular/common/http';
+import { map, Observable } from "rxjs";
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class ApiService {
+    private apiUrl = 'https://api.crm.xonis.net/api/json/example/task/contactCards';
+    private token = '1e70676a3c6787bf6c4656a93e19d0ffea085b7b-aaabff5bc3f926159ce38d51ba7bf525-1682681240';
+
+    constructor(private http: HttpClient) { }
+    
+    getContacts(): Observable<any> {
+        const options = {
+            params: {
+                token: this.token
+            }
+        };
+
+        return this.http.get<any>(this.apiUrl, options).pipe(
+            map((data: any) => {
+                const contacts = data.contacts.map((contact: any) => {
+                    return {
+                        ...contact,
+                        avatar: `./assets/avatar/${contact.id}.jpg`
+                    };
+                });
+                return {
+                    ...data,
+                    contacts
+                };
+            })
+        );
+    }
+}
+
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'contact';
+  title(title: any) {
+    throw new Error('Method not implemented.');
+  }
+  total: string | undefined;
+  contacts: any = [];
+
+  constructor(private apiService: ApiService) { }
+
+  getContacts() {
+    this.apiService.getContacts().subscribe((data: any) => {
+      this.contacts = data.contacts;
+      this.total = data.total
+    });
+  }
 }
